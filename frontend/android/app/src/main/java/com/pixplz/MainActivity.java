c8e16e31f337802837aa807fbac75008be51c1c7
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2019-02-12 00:41:52 -0500

    Include generated ios and android assets

---
+++ frontend/android/app/src/main/java/com/pixplz/MainActivity.java
@@ -0,0 +1,8 @@
+package com.pixplz;
+
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
+
+// public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
+}
